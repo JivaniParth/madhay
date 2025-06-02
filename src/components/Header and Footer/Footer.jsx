b33d8e85b228c86a29cc7b0f5b0f09754de6fc66
Parth Jivani <149225568+JivaniParth@users.noreply.github.com>
2025-06-02 15:26:49 +0530

    Create Footer.jsx

---
+++ src/components/Header and Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p className="author">Made in ❤️ by Parth S Jivani</p>
+      <br />
+      <p>Copyright &copy; 2023</p>
+    </footer>
+  );
+}
